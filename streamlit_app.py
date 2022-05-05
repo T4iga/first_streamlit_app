dca19a0f791e879a1771779952af7b726e55716e
T4iga <38498928+T4iga@users.noreply.github.com>
2022-05-05 13:26:03 +0200

    Update streamlit_app.py
    Refix the "typo"

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,6 +27,6 @@ import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 
 # take the json version of the response and normalize it
-fruityvice_normalized = pandas.json_normalized(fruityvice.response.json())
+fruityvice_normalized = pandas.json_normalize(fruityvice.response.json())
 # output it to the screen as a table
 streamlit.dataframe(fruityvice_normalized)
