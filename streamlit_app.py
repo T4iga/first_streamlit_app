9bd6133c1c3a4cfc6d6803e995523619a11e2870
T4iga <38498928+T4iga@users.noreply.github.com>
2022-05-05 16:52:11 +0200

    Update streamlit_app.py
    Add field for user to add fruit

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,7 @@ streamlit.dataframe(fruits_to_show)
 
 # New Section to dispay fruityvice api response
 streamlit.header('Fruityvice Fruit Advice!')
-fruit_choice = streamlit.text_input('What fruit woould you like information about?', 'Kiwi')
+fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
 streamlit.write('The user entered', fruit_choice)
 
 import requests
@@ -40,6 +40,8 @@ my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT * from fruit_load_list")
 my_data_rows = my_cur.fetchall()
+add_my_fruit = streamlit.text_input('What fruit would you like to add to the list', 'Jackfruit')
+streamlit.write('The user added', add_my_fruit)
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
