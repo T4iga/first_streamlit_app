ba15b2c7eb1bcc50474f6eeaa2eae939409242cd
T4iga <38498928+T4iga@users.noreply.github.com>
2022-05-06 11:30:13 +0200

    Update streamlit_app.py
    searching for the right place of the button

--- streamlit_app.py
+++ streamlit_app.py
@@ -55,18 +55,18 @@ if streamlit.button('Get Fruit Load List'):
     streamlit.dataframe(my_data_rows)
     
 # Don't run any code past here while troubleshooting
-streamlit.stop()
+#streamlit.stop()
 
 def insert_row_snowflake(new_fruit):
     with my_cnx.cursor() as my_cur:
         my_cur.execute("INSERT into fruit_load_list values ('" + new_fruit +"' )")
         return "Thanks for adding " + new_fruit
 
-    
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 if streamlit.button('Add Fruit to List'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"]) 
     back_from_function = insert_row_snowflake(add_my_fruit)
+    my_cnx.close()
     streamlit.text(back_from_function)
 
 if streamlit.button('Get Fruit List'):
